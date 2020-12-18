12fd2b5863e29197970743a5e931c9aa59cdc832
Shu Wei Chou-Chen <57328890+shuwei325@users.noreply.github.com>
2020-12-17 19:15:23 -0600

    Adding some basic graphs

---
+++ 1.visualization.R
@@ -0,0 +1,69 @@
+#library("animation")
+library("dplyr")
+library("ggplot2")
+library("gstat")
+library("maps")
+library("mapdata")
+library("STRbook")
+library("grid")
+library("gridExtra")
+
+
+#loading data
+load("ts_RegionalMensualMonson.Rdata")
+names(ts_Regional)
+
+head(ts_Regional)
+
+ts_Regional$lon <- ts_Regional$lon-360
+
+ts_Regional$ts <- ts_Regional$ts- 273.15 
+
+ts_Regional1998 <- filter(ts_Regional, Year %in% c(1998),
+                                       Month %in% 1:12) 
+
+Month_label <- c("ene","feb","mar","abr","may","jun","jul","ago","sept","oct","nov","dic")
+names(Month_label) <- as.character(1:12)
+
+plot1 <- ggplot(ts_Regional1998) +             # plot points
+  geom_point(aes(x = lon,y = lat,       # lon and lat
+                 colour = ts),           # attribute color
+             size = 2.5)+                # make all points larger
+  col_scale(name = "Temperatura (°C)") +
+  xlab("Longitud") +             # x-axis label
+  ylab("Latitud") +              # y-axis label
+  geom_path(data = map_data("worldHires"),   # add map
+            aes(x = long, y = lat, group = group), 
+            col=1) +
+  facet_wrap(~Month, nrow = 3,labeller = labeller( Month = Month_label)) +        # facet by time
+  coord_fixed(xlim = c(-122, -92),
+              ylim = c(20, 42))  +      # zoom in
+  theme_bw()     
+
+plot1
+
+
+# Algunos gráficos para la página -----------------------------------------
+
+pdf("grafico_pagina2.pdf")
+print(plot1)
+dev.off()
+
+png(file = "mapa1.png", bg = "transparent")
+print(plot1)
+dev.off()
+
+library(Cairo)
+Cairo::Cairo(
+  30, #length
+  30, #width
+  file = paste("mapa2", ".png", sep = ""),
+  type = "png", #tiff
+  bg = "transparent", #white or transparent depending on your requirement 
+  dpi = 300,
+  units = "cm" #you can change to pixels etc 
+)
+print(plot1)
+dev.off()
+
+
---
+++ 2.ts.R
@@ -0,0 +1,136 @@
+library(maps)
+library(gstat)
+library(maptools)
+library(spacetime)
+library(RColorBrewer)
+library(fields)
+library(mapdata)
+library(geosphere)
+library(pdist)
+
+library(tidyverse)
+library(fracdiff)
+library(arfima)
+library(ggplot2)
+library(ggcorrplot)
+library(corrplot)
+library(RColorBrewer)
+library(forecast)
+library(fpp2)
+library(ggfortify)
+library(dplR)
+library(wavethresh)
+library(WaveletComp)
+library(astsa)
+
+
+setwd("~/MRA_ST/MRA2_mensual")
+getwd()
+
+load('Temp_mensual.Rdata')
+#load('Prec_mensual.Rdata')
+
+var_regional <- hh[,,"Y",drop=FALSE]
+class(var_regional)
+
+# 1. Map (5 locations) --------------------------------------------------
+
+aprox_select_locations <- c(1237, 626, 1859, 1843, 634) # 5 locations from the previous script.
+stplot(var_regional[aprox_select_locations,],mode="ts")
+
+datos.ts<-var_regional[aprox_select_locations,,drop=TRUE]
+y1<-ts(datos.ts[1,,"Y"]$Y,frequency = 12, start=c(1981,1))
+y2<-ts(datos.ts[2,,"Y"]$Y,frequency = 12, start=c(1981,1))
+y3<-ts(datos.ts[3,,"Y"]$Y,frequency = 12, start=c(1981,1))
+y4<-ts(datos.ts[4,,"Y"]$Y,frequency = 12, start=c(1981,1))
+y5<-ts(datos.ts[5,,"Y"]$Y,frequency = 12, start=c(1981,1))
+
+datos.ts<- cbind(y1,y2,y3,y4,y5)
+
+#pdf("grafico_pagina3.pdf")
+autoplot(datos.ts)
+#dev.off()
+
+autoplot(datos.ts, facets = TRUE)
+
+round(corr<-cor(datos.ts),2)
+corrplot(corr, method="circle")
+
+par(mfrow=c(2,3))
+acf(y1,lag.max=50,ci=0.95)
+acf(y2,lag.max=50,ci=0.95)
+acf(y3,lag.max=50,ci=0.95)
+acf(y4,lag.max=50,ci=0.95)
+acf(y5,lag.max=50,ci=0.95)
+
+
+# Location 1 --------------------------------------------------------------
+
+par(mfrow=c(3,1))
+ts.plot(y1)
+acf(y1,lag.max=50,ci=0.95)
+pacf(y1,lag.max=50,ci=0.95)
+
+
+#diff 1
+dy1<-diff(y1)
+
+par(mfrow=c(3,1))
+ts.plot(dy1)
+acf(dy1,lag.max=500,ci=0.95)
+pacf(dy1,lag.max=500,ci=0.95)
+
+
+fdGPH(y1, bandw=.9)
+
+
+# Spectral analysis
+
+par(mfrow=c(1,2))
+raw.spec1 <- spec.pgram(y1, taper = 0)
+plot(raw.spec1, log = "no")
+
+max(raw.spec1$spec)
+(freq<-raw.spec1$freq[(raw.spec1$spec==max(raw.spec1$spec))])
+(periodo<-1/freq)
+
+ts.plot(datos.ts, col=1:9)
+spec = mvspec(datos.ts, spans=c(3,3), taper=.1)
+
+
+# Wavelets analysis
+
+datos.ts<- as.data.frame(datos.ts)
+colnames(datos.ts)<-c("y1","y2","y3","y4","y5")
+
+my.w1 <- analyze.wavelet(datos.ts, "y1",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 100,make.pval = TRUE, n.sim = 10)
+my.w2 <- analyze.wavelet(datos.ts, "y2",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 100,make.pval = TRUE, n.sim = 10)
+my.w3 <- analyze.wavelet(datos.ts, "y3",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 100,make.pval = TRUE, n.sim = 10)
+my.w4 <- analyze.wavelet(datos.ts, "y4",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 100,make.pval = TRUE, n.sim = 10)
+my.w5 <- analyze.wavelet(datos.ts, "y5",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 100,make.pval = TRUE, n.sim = 10)
+
+wt.image(my.w1, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w2, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w3, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w4, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w5, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+
+
+
+
+
