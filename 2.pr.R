8c1d831f0856fa7b87391116a3762f4e464a456d
Shu Wei Chou-Chen <57328890+shuwei325@users.noreply.github.com>
2021-01-25 12:29:32 -0600

    adding EOF and plots

--- 2.pr.R
+++ 2.pr.R
@@ -33,6 +33,94 @@ load('Prec_mensual.Rdata')
 var_regional <- hh[,,"Y",drop=FALSE]
 class(var_regional)
 
+
+# 0. Maps -----------------------------------------------------------------
+
+pr.eof1<-eof(var_regional, "spatial", returnEOFs = FALSE, scale.=TRUE) #correlation
+pr.eof2<-eof(var_regional, "spatial", returnEOFs = FALSE, scale.=FALSE) #covariance
+plot(pr.eof1)
+plot(pr.eof2)
+summary(pr.eof1)$importance[,1:10]
+summary(pr.eof2)$importance[,1:10]
+
+pr.eof2a<-eof(var_regional, "spatial", returnEOFs = TRUE, scale.=FALSE) #covariance
+
+str(pr.eof2a)
+pr.comp1<-pr.eof2a[,"EOF1"]
+pr.comp2<-pr.eof2a[,"EOF2"]
+
+spplot(pr.comp1)
+spplot(pr.comp2)
+
+coordinates<-coordinates(pr.comp1)
+EOF1a<-data.frame(coordinates,score=pr.comp1$"EOF1",componente=rep(1,nrow(coordinates)))
+EOF2a<-data.frame(coordinates,score=pr.comp2$"EOF2",componente=rep(2,nrow(coordinates)))
+
+plot(EOF1a$score,EOF2a$score)
+
+plot1 <- {ggplot(EOF1a) +             # plot points
+    geom_point(aes(x = lon,y = lat,       # lon and lat
+                   colour = score),           # attribute color
+               size = 2.5)+                # make all points larger
+    col_scale(name = "Score") +
+    xlab("Longitud") +             # x-axis label
+    ylab("Latitud") +              # y-axis label
+    labs(title = "", 
+         subtitle = "", 
+         caption = "") +
+    geom_path(data = map_data("worldHires"),   # add map
+              aes(x = long, y = lat, group = group), 
+              col=1) +
+    coord_fixed(xlim = c(-122, -92),
+                ylim = c(20, 42))  +      # zoom in
+    theme_bw()     
+}
+plot2 <- {ggplot(EOF2a) +             # plot points
+    geom_point(aes(x = lon,y = lat,       # lon and lat
+                   colour = score),           # attribute color
+               size = 2.5)+                # make all points larger
+    col_scale(name = "Score") +
+    xlab("Longitud") +             # x-axis label
+    ylab("Latitud") +              # y-axis label
+    labs(title = "", 
+         subtitle = "", 
+         caption = "") +
+    geom_path(data = map_data("worldHires"),   # add map
+              aes(x = long, y = lat, group = group), 
+              col=1) +
+    coord_fixed(xlim = c(-122, -92),
+                ylim = c(20, 42))  +      # zoom in
+    theme_bw()     
+}
+plot1
+plot2
+
+Cairo::Cairo(
+  15, #length
+  15, #width
+  file = paste("pr_EOF1", ".png", sep = ""),
+  type = "png", #tiff
+  bg = "transparent", #white or transparent depending on your requirement 
+  dpi = 300,
+  units = "cm" #you can change to pixels etc 
+)
+plot1
+dev.off()
+
+Cairo::Cairo(
+  15, #length
+  15, #width
+  file = paste("pr_EOF2", ".png", sep = ""),
+  type = "png", #tiff
+  bg = "transparent", #white or transparent depending on your requirement 
+  dpi = 300,
+  units = "cm" #you can change to pixels etc 
+)
+plot2
+dev.off()
+
+
+
 # 1. Map (5 locations) --------------------------------------------------
 
 aprox_select_locations <- c(1237, 626, 1859, 1843, 634) # 5 locations from the previous script.
@@ -57,8 +145,8 @@ Cairo::Cairo(
   units = "cm" #you can change to pixels etc 
 )
 
-autoplot(datos.ts, xlab = "Tiempo", ylab = "Precipitación") +
-  scale_colour_discrete("Locación", labels = c("1", "2", "3","4","5"))
+autoplot(datos.ts, xlab = "Time", ylab = "Precipitation") +
+  scale_colour_discrete("Location", labels = c("1", "2", "3","4","5"))
 
 dev.off()
 
@@ -68,11 +156,11 @@ round(corr<-cor(datos.ts),2)
 corrplot(corr, method="circle")
 
 
-p1<-ggAcf(y1, lag.max = 50) + ggtitle("Locación 1")
-p2<-ggAcf(y2, lag.max = 50) + ggtitle("Locación 2")
-p3<-ggAcf(y3, lag.max = 50) + ggtitle("Locación 3")
-p4<-ggAcf(y4, lag.max = 50) + ggtitle("Locación 4")
-p5<-ggAcf(y5, lag.max = 50) + ggtitle("Locación 5")
+p1<-ggAcf(y1, lag.max = 50) + ggtitle("Location 1")
+p2<-ggAcf(y2, lag.max = 50) + ggtitle("Location 2")
+p3<-ggAcf(y3, lag.max = 50) + ggtitle("Location 3")
+p4<-ggAcf(y4, lag.max = 50) + ggtitle("Location 4")
+p5<-ggAcf(y5, lag.max = 50) + ggtitle("Location 5")
 
 Cairo::Cairo(
   15, #length
@@ -116,6 +204,11 @@ plot(raw.spec1, log = "no")
 
 ts.plot(datos.ts, col=1:9)
 spec = mvspec(datos.ts, spans=c(3,3), taper=.1)
+spec = mvspec(datos.ts, spans=c(3,3), taper=.1,col=1:5,lty=1:5)
+legend("topright",
+       c("location 1","location 2","location 3","location 4","location 5"),
+       col=1:5,lty=1:5)
+
 
 # Spectral analysis
 
--- 2.ts.R
+++ 2.ts.R
@@ -33,6 +33,93 @@ load('Temp_mensual.Rdata')
 var_regional <- hh[,,"Y",drop=FALSE]
 class(var_regional)
 
+
+
+# 0. Maps -----------------------------------------------------------------
+
+ts.eof1<-eof(var_regional, "spatial", returnEOFs = FALSE, scale.=TRUE) #correlation
+ts.eof2<-eof(var_regional, "spatial", returnEOFs = FALSE, scale.=FALSE) #covariance
+plot(ts.eof1)
+plot(ts.eof2)
+summary(ts.eof1)$importance[,1:10]
+summary(ts.eof2)$importance[,1:10]
+
+ts.eof2a<-eof(var_regional, "spatial", returnEOFs = TRUE, scale.=FALSE) #covariance
+
+str(ts.eof2a)
+ts.comp1<-ts.eof2a[,"EOF1"]
+ts.comp2<-ts.eof2a[,"EOF2"]
+
+spplot(ts.comp1)
+spplot(ts.comp2)
+
+coordinates<-coordinates(ts.comp1)
+EOF1a<-data.frame(coordinates,score=ts.comp1$"EOF1",componente=rep(1,nrow(coordinates)))
+EOF2a<-data.frame(coordinates,score=ts.comp2$"EOF2",componente=rep(2,nrow(coordinates)))
+
+plot(EOF1a$score,EOF2a$score)
+
+plot1 <- {ggplot(EOF1a) +             # plot points
+  geom_point(aes(x = lon,y = lat,       # lon and lat
+                 colour = score),           # attribute color
+             size = 2.5)+                # make all points larger
+  col_scale(name = "Score") +
+  xlab("Longitud") +             # x-axis label
+  ylab("Latitud") +              # y-axis label
+  labs(title = "", 
+       subtitle = "", 
+       caption = "") +
+  geom_path(data = map_data("worldHires"),   # add map
+            aes(x = long, y = lat, group = group), 
+            col=1) +
+  coord_fixed(xlim = c(-122, -92),
+              ylim = c(20, 42))  +      # zoom in
+  theme_bw()     
+}
+plot2 <- {ggplot(EOF2a) +             # plot points
+    geom_point(aes(x = lon,y = lat,       # lon and lat
+                   colour = score),           # attribute color
+               size = 2.5)+                # make all points larger
+    col_scale(name = "Score") +
+    xlab("Longitud") +             # x-axis label
+    ylab("Latitud") +              # y-axis label
+    labs(title = "", 
+         subtitle = "", 
+         caption = "") +
+    geom_path(data = map_data("worldHires"),   # add map
+              aes(x = long, y = lat, group = group), 
+              col=1) +
+    coord_fixed(xlim = c(-122, -92),
+                ylim = c(20, 42))  +      # zoom in
+    theme_bw()     
+}
+plot1
+plot2
+
+Cairo::Cairo(
+  15, #length
+  15, #width
+  file = paste("temp_EOF1", ".png", sep = ""),
+  type = "png", #tiff
+  bg = "transparent", #white or transparent depending on your requirement 
+  dpi = 300,
+  units = "cm" #you can change to pixels etc 
+)
+plot1
+dev.off()
+
+Cairo::Cairo(
+  15, #length
+  15, #width
+  file = paste("temp_EOF2", ".png", sep = ""),
+  type = "png", #tiff
+  bg = "transparent", #white or transparent depending on your requirement 
+  dpi = 300,
+  units = "cm" #you can change to pixels etc 
+)
+plot2
+dev.off()
+
 # 1. Map (5 locations) --------------------------------------------------
 
 aprox_select_locations <- c(1237, 626, 1859, 1843, 634) # 5 locations from the previous script.
@@ -57,8 +144,8 @@ Cairo::Cairo(
   units = "cm" #you can change to pixels etc 
 )
 
-autoplot(datos.ts, xlab = "Tiempo", ylab = "temperatura") +
-  scale_colour_discrete("Locación", labels = c("1", "2", "3","4","5"))
+autoplot(datos.ts, xlab = "Time", ylab = "Temperature") +
+  scale_colour_discrete("Location", labels = c("1", "2", "3","4","5"))
 
 dev.off()
 
@@ -67,11 +154,11 @@ autoplot(datos.ts, facets = TRUE)
 round(corr<-cor(datos.ts),2)
 corrplot(corr, method="circle")
 
-p1<-ggAcf(y1, lag.max = 50) + ggtitle("Locación 1")
-p2<-ggAcf(y2, lag.max = 50) + ggtitle("Locación 2")
-p3<-ggAcf(y3, lag.max = 50) + ggtitle("Locación 3")
-p4<-ggAcf(y4, lag.max = 50) + ggtitle("Locación 4")
-p5<-ggAcf(y5, lag.max = 50) + ggtitle("Locación 5")
+p1<-ggAcf(y1, lag.max = 50) + ggtitle("Location 1")
+p2<-ggAcf(y2, lag.max = 50) + ggtitle("Location 2")
+p3<-ggAcf(y3, lag.max = 50) + ggtitle("Location 3")
+p4<-ggAcf(y4, lag.max = 50) + ggtitle("Location 4")
+p5<-ggAcf(y5, lag.max = 50) + ggtitle("Location 5")
 
 Cairo::Cairo(
   15, #length
@@ -128,7 +215,10 @@ Cairo::Cairo(
   dpi = 300,
   units = "cm" #you can change to pixels etc 
 )
-spec = mvspec(datos.ts, spans=c(3,3), taper=.1,main="")
+spec = mvspec(datos.ts, spans=c(3,3), taper=.1,col=1:5,lty=1:5)
+legend("topright",
+       c("location 1","location 2","location 3","location 4","location 5"),
+       col=1:5,lty=1:5)
 dev.off()
 
 # Wavelets analysis
